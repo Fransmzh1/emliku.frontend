35eb3c349f8d67c6f4835e0845bb9bdde90d6d4c
frans.mazhar <frans.mazhar@gmail.com>
2023-02-16 22:13:32 +0700

    update

--- src/components/AppSidebar.js
+++ src/components/AppSidebar.js
@@ -1,19 +1,13 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 
-import { CSidebar, CSidebarBrand, CSidebarNav, CSidebarToggler } from '@coreui/react'
+import { CSidebar, CSidebarBrand, CSidebarToggler } from '@coreui/react'
 import CIcon from '@coreui/icons-react'
-
-import { AppSidebarNav } from './AppSidebarNav'
 
 import { logoNegative } from 'src/assets/brand/logo-negative'
 import { sygnet } from 'src/assets/brand/sygnet'
 
-import SimpleBar from 'simplebar-react'
 import 'simplebar/dist/simplebar.min.css'
-
-// sidebar nav config
-import navigation from '../_nav'
 
 const AppSidebar = () => {
   const dispatch = useDispatch()
@@ -33,11 +27,6 @@ const AppSidebar = () => {
         <CIcon className="sidebar-brand-full" icon={logoNegative} height={35} />
         <CIcon className="sidebar-brand-narrow" icon={sygnet} height={35} />
       </CSidebarBrand>
-      <CSidebarNav>
-        <SimpleBar>
-          <AppSidebarNav items={navigation} />
-        </SimpleBar>
-      </CSidebarNav>
       <CSidebarToggler
         className="d-none d-lg-flex"
         onClick={() => dispatch({ type: 'set', sidebarUnfoldable: !unfoldable })}
---
+++ src/components/alert/Alert.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Alert = props => {
+  return (
+    <div>Alert</div>
+  )
+}
+
+Alert.propTypes = {}
+
+export default Alert
